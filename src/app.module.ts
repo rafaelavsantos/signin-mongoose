3f774a025da1278f5a23c4deeccc60845de1ecee
Rafaela Santos <rafaelasantosminios@gmail.com>
2025-12-10 13:11:58 -0300

    feat: removing unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [UsersModule, AuthModule, MongooseModule.forRoot(process.env.MONGO_URI!)],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule { }
